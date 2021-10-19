bbda5903e727be6ecb55a7ba0d9409dbcbb32e1f
Raphaela Vergud <r.vergud@c02g10evml86.fritz.box>
2021-10-19 11:34:53 +0200

    first django commit

---
+++ django_blog/blog/views.py
@@ -0,0 +1,20 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+from .models import Blog
+# Create your views here.
+
+def home(request):
+    blog_posts = Blog.objects.all()
+    blog_list_html = ''
+    for blog in blog_posts:
+        blog_list_html += f'<p><a href="/blog/{blog.id}/">{blog.title}</a></p>'
+    print(blog_posts)
+    html = f'<html><body>{blog_list_html}</body></html>'
+    return HttpResponse(html)
+
+def blog_post(request, id=1):
+    blog_post = Blog.objects.get(id=id)
+    html = f"<html><body><h1>{blog_post.title}</h1>{blog_post.date}<p>{blog_post.body}</p></body></html>"
+    return HttpResponse(html)
+
