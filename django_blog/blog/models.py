6d08419c015308abbe67bcba18ce9f203d13dabf
Raphaela Vergud <r.vergud@c02g10evml86.fritz.box>
2021-10-20 10:02:53 +0200

    changed the views.py and added templates

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Blog(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
@@ -7,5 +8,3 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
-
-
