7e4edcfaa1ea79f6e757ff6c49309f889270fcfb
Raphaela Vergud <r.vergud@c02g10evml86.fritz.box>
2021-10-28 14:36:33 +0200

    no

--- django_blog/git.Repo.py
+++ django_blog/git.Repo.py
@@ -3,6 +3,6 @@ from git import Repo
 # rorepo is a Repo instance pointing to the git-python repository.
 # For all you know, the first argument to Repo is a path to the repository
 # you want to work with
-repo = Repo('')
+repo = Repo('/Users/r.vergud/Desktop/code/projects/django_blog/django_blog')
 assert not repo.bare
 
