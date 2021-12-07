11c26f1b7baae7ba2001fe718327b6c742f2c807
Raphaela Vergud <raphaelavergud@icloud.com>
2021-12-07 11:27:46 +0100

    added a test that does not work yet

--- django_blog/blog/tests.py
+++ django_blog/blog/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+#!/usr/bin/env python3
 
-# Create your tests here.
+from django.test import TestCase
+from blog.models import Blog
+
+
+class SimpleTest(TestCase):
+    def setUp(self):
+        self.user = Blog.objects.create_superuser(username='testuser', password='password')
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_add_blog_post(self):
+        self.client.login(username='testuser', password='password')
+        response = self.client.post('/admin/blog/django_blog/add/', {'title': 'TEST TITLE', 'body': 'SOME BODY', '_save': 'SAVE'})
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get('/')
+        self.assertTrue('TEST TITLE' in str(response.content))
