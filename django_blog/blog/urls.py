6a117ed2b52ed88dfe642ca2e954404a8af5a166
Raphaela Vergud <raphaelavergud@icloud.com>
2022-01-10 16:27:47 +0100

    different CSS in a static file

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+# first argument in urlpatterns is the path and second argument is which function that should be called from views for that path
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("blog/<int:id>/", views.blog_post, name="blog_post"),
+    path("update_server/", views.update, name="update"),
+    path("item/<item_id>/", views.item),
+    path("register/", views.register_request, name="register"),
+    path("login/", views.login_request, name="login"),
+    path("logout/", views.logout_request, name= "logout"),
+]
+
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -15,17 +15,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from blog import views
 
+# first argument in urlpatterns is the path and second argument is which function that should be called for that path
+
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("blog/<int:id>/", views.blog_post, name="blog_post"),
     path("admin/", admin.site.urls),
-    path("update_server/", views.update, name="update"),
-    path("item/<item_id>", views.item),
-    path("register", views.register_request, name="register"),
-    path("login", views.login_request, name="login"),
-    path("logout", views.logout_request, name= "logout"),
+    path("", include("blog.urls"))
 ]
