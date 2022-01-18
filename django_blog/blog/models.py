98b037d90aa4918a59bed1ba40e7a2a08bd34570
Raphaela Vergud <raphaelavergud@icloud.com>
2022-01-18 23:24:25 +0100

    fixed one thing, more things need fixing

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -63,7 +63,7 @@ class NewUser(AbstractBaseUser, PermissionsMixin):
     start_date = models.DateTimeField(default=datetime.datetime.now)
     about = models.TextField(_("about"), max_length=500, blank=True)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     objects = CustomAccountManager()
 
