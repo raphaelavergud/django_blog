a4d30c84278d4b41d6aba2c7786d2c459270bb9e
Raphaela Vergud <raphaelavergud@icloud.com>
2022-02-02 14:10:27 +0100

    added a new aplication

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
+    "calculator",
     "markdownify.apps.MarkdownifyConfig",
     "crispy_forms",
 ]
