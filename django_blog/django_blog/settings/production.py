6ee8f58e57871ab40a1fc34448611063276ea214
Raphaela Vergud <raphaelavergud@icloud.com>
2022-03-22 15:58:17 +0100

    change to staticfiles_storage

--- django_blog/django_blog/settings/production.py
+++ django_blog/django_blog/settings/production.py
@@ -4,6 +4,7 @@ DEBUG = False
 ALLOWED_HOSTS = ["raphaelavergud.pythonanywhere.com", "*", "3.127.243.35", "raphaela.lundinfo.com"]
 
 STATIC_ROOT = "/home/ubuntu/static"
+STATICFILES_STORAGE = “django.contrib.staticfiles.storage.ManifestStaticFilesStorage”
 
 DATABASES = {
     "default": {
